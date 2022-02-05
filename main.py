bdcd45282ada635a1a0506d5fc3e48b84a39cda6
AnishRaj <64763101+arajv11@users.noreply.github.com>
2022-02-05 15:54:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import yagmail
+import os
+import pandas
+
+sender = 'arajvpythontest1@gmail.com'
+
+yag = yagmail.SMTP(user=sender,password=os.getenv('PASSWORD'))
+
+df = pandas.read_csv('contacts.csv')
+print(df)
+
+def generate_file(filename, content):
+  with open(filename, 'w') as file:
+    file.write(str(content))
+
+for index, row in df.iterrows():
+  name = row['name']
+  filename = name + ".txt"
+  amount = row['amount']
+  receiver_email = row['email']
+
+  generate_file(filename, amount)
+
+  subject = "This is the subject"
+  contents = [f"""Hi {name} you have to pay {amount} Bill is attached!""", filename]
+  yag.send(to=receiver_email, subject=subject, contents=contents)
+  print("Email sent!")
